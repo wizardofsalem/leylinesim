fe3d11bacdfc06f0a3ea872e02ad27745fa53bbf
Cal Lees <caltriestocode@gmail.com>
2026-09-07 02:57:21 +0100

    make leynode an aggregate

--- leylib/include/leylib/LeyNode.h
+++ leylib/include/leylib/LeyNode.h
@@ -1,22 +1,12 @@
 #pragma once
-#include "leylib/FlowRndGenerator.h"
 #include <cstdint>
-#include <leylib/NodeHandle.h>
-#include <memory>
 #include <vector>
 
-class LeyNode {
-public:
-  LeyNode() = delete;
+#include <leylib/NodeHandle.h>
 
-  LeyNode(NodeHandle handle, FlowRndGenerator &flowGenerator);
-  LeyNode(NodeHandle handle, float flowAmount);
-  virtual ~LeyNode() = default;
-  void increaseGeneration();
-
-private:
-  NodeHandle handle_;
-  std::vector<uint16_t> adjacentNodes_{};
-  uint16_t nodeId_;
-  float flow_;
+struct LeyNode {
+  NodeHandle handle{};
+  std::vector<uint32_t> adjacentNodes{};
+  float flow{};
+  bool alive{};
 };
