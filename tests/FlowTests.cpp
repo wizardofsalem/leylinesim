dcb7f7a270803ce2766b5f935789a0405fa5a99e
Cal Lees <caltriestocode@gmail.com>
2026-09-07 02:14:00 +0100

    tests

---
+++ tests/FlowTests.cpp
@@ -0,0 +1,4 @@
+#include <gtest/gtest.h>
+#include <leylib/LeyGraph.h>
+
+TEST(FlowTests, ShouldSinkFlow) {}
---
+++ tests/NodeIdGeneratorTest.cpp
@@ -0,0 +1,28 @@
+#include <gtest/gtest.h>
+#include <leylib/NodeIdGenerator.h>
+
+TEST(NodeIdGenerator, GeneratesSequentialIdsFromZero) {
+  NodeIdGenerator gen;
+  EXPECT_EQ(gen.generateId(), 0);
+  EXPECT_EQ(gen.generateId(), 1);
+  EXPECT_EQ(gen.generateId(), 2);
+}
+
+TEST(NodeIdGenerator, ReusesFreedId) {
+  NodeIdGenerator gen;
+  const uint16_t a = gen.generateId();
+  const uint16_t b = gen.generateId();
+  (void)a;
+
+  gen.freeId(b);
+  EXPECT_EQ(gen.generateId(), b);
+}
+
+TEST(NodeIdGenerator, ContinuesAfterReuse) {
+  NodeIdGenerator gen;
+  gen.generateId();
+  const uint16_t one = gen.generateId();
+  gen.freeId(one);
+  EXPECT_EQ(gen.generateId(), one);
+  EXPECT_EQ(gen.generateId(), 2);
+}
