566379a05d83d674d123b49fb68c9d62b08cb01f
Cal Lees <caltriestocode@gmail.com>
2026-09-07 02:14:55 +0100

    wrapper class to generate values from normal dist

---
+++ leylib/FlowRndGenerator.cpp
@@ -0,0 +1,19 @@
+#include <leylib/FlowRndGenerator.h>
+
+FlowRndGenerator::FlowRndGenerator(double mean, double stddev)
+    : engine_(std::random_device{}()), dist_(mean, stddev) {}
+
+FlowRndGenerator::FlowRndGenerator(double mean, double stddev, uint32_t seed)
+    : engine_(seed), dist_(mean, stddev) {}
+
+double FlowRndGenerator::next() { return dist_(engine_); }
+
+double FlowRndGenerator::mean() const { return dist_.mean(); }
+
+double FlowRndGenerator::stddev() const { return dist_.stddev(); }
+
+void FlowRndGenerator::setParams(double mean, double stddev) {
+  dist_.param(std::normal_distribution<double>::param_type{mean, stddev});
+}
+
+void FlowRndGenerator::seed(uint32_t seed) { engine_.seed(seed); }
---
+++ leylib/include/leylib/FlowRndGenerator.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <cstdint>
+#include <random>
+
+class FlowRndGenerator {
+public:
+  FlowRndGenerator(double mean, double stddev);
+
+  FlowRndGenerator(double mean, double stddev, uint32_t seed);
+
+  ~FlowRndGenerator() = default;
+
+  double next();
+
+  double mean() const;
+  double stddev() const;
+  void setParams(double mean, double stddev);
+
+  void seed(uint32_t seed);
+
+private:
+  std::mt19937 engine_;
+  std::normal_distribution<double> dist_;
+};
